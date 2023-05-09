b10c8cc245f2353dfe3f5cbd6e231082a23ced7d
Kris Nuttycombe <kris@nutty.land>
2023-05-08 22:09:29 -0600

    Fix order of `isValidSaplingAddress` arguments.

--- src/Lrzhs.hs
+++ src/Lrzhs.hs
@@ -6,5 +6,5 @@ import Foreign.C.String (withCString)
 import Lrzhs.Ffi (networkId, rs_is_valid_sapling_address)
 import Lrzhs.Types (Network)
 
-isValidSaplingAddress :: Text -> Network -> IO Bool
-isValidSaplingAddress t n = fmap (\(CBool b) -> b /= 0) $ withCString (unpack t) (\cs -> rs_is_valid_sapling_address cs (networkId n))
+isValidSaplingAddress :: Network -> Text -> IO Bool
+isValidSaplingAddress n t = fmap (\(CBool b) -> b /= 0) $ withCString (unpack t) (\cs -> rs_is_valid_sapling_address cs (networkId n))
