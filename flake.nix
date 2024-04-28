65ee43717492fe6f2e086c331439b9d61abcdfc7
Kris Nuttycombe <kris@nutty.land>
2024-04-28 11:13:42 -0600

    Upgrade to nixpkgs 23.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake for building lrzhs, a set of Haskell bindings for librustzcash";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/release-22.11;
+    nixpkgs.url = github:NixOS/nixpkgs/release-23.05;
     flake-utils.url = "github:numtide/flake-utils";
     crane = {
       inputs.nixpkgs.follows = "nixpkgs";
